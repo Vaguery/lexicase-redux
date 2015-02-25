5cf4fbf2c9ee5a821d8733ea844306fa99a632a6
Nic McPhee <mcphee@morris.umn.edu>
2015-02-24 23:49:33 -0500

    Added the Github URL to `project.clj`.
    I figured that was better than "http://example.com/FIXME" :-).

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lexicase-redux "0.1.0-SNAPSHOT"
   :description "writing a lexicase selection operator so I understand it better"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/Vaguery/lexicase-redux"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
